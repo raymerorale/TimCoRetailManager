0bbdd24173b10438fd10f5f356307af81563177c
John Raymer Orale <john.raymer.orale@accenture.com>
2022-01-16 19:35:03 +0800

    Add Sales Page

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,97 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+        private BindingList<string> _cart;
+        private string _itemQuantity;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set 
+            { 
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set 
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal 
+        { 
+            get 
+            {
+                return "$0.00";
+            } 
+        }
+        public string Tax
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+
+        public bool CanAddToCart()
+        {
+            return false;
+        }
+
+        public void AddToCart()
+        {
+
+        }
+        public bool CanRemoveFromCart()
+        {
+            return false;
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+        public bool CanCheckOut()
+        {
+            return false;
+        }
+
+        public void CheckOut()
+        {
+
+        }
+
+    }
+}
